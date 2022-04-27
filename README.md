24a76ed9b098b979d417eb325f3792e79b433efa
The0n1y5pace4ce <spaceacethe625@gmail.com>
2022-04-27 17:02:15 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### KSP Part Explosion
+
+# This is a small plugin I have made (also my first) to simulate a failure, just press left control for a random part on your craft to explode# KSPPartExplosion
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[assembly: AssemblyTitle("KSPPartExplosion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KSPPartPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
